c7bfb0ce726949554ab2a45fb1c83d25be886f34
Haruka Murakami <2111140025k@ed.fuk.kindai.ac.jp>
2023-11-22 08:34:33 +0000

    X

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "e4219d157d9e451c84a29906d6f1ff9b.vfs.cloud9.us-east-1.amazonaws.com"
 end
